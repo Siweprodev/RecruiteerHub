4e38f156343654115f2099a967e59689593dcecf
adelaide20 <adelaiderorisang@gmail.com>
2023-10-20 08:17:13 +0200

    created user model

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// set port, listen for requests
+const PORT = process.env.PORT || 6060;
+
+var corsOptions = {
+    origin: '*',
+    allowCredentials: "true",
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to RecruitHub application." });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
